11a69388f00b6762f8d87d0385006e26ab818be2
stpwin <isysforce@gmail.com>
2019-11-13 08:09:26 +0700

    second one

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Chart } from 'react-charts'
+import React, { Component } from "react";
+import "./App.css";
+import { Chart } from "react-charts";
 
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwfvkoffX7MNGrWrf7nYYQ9wmOwS5_yaI",
@@ -14,107 +14,56 @@ const firebaseConfig = {
   appId: "1:796798924451:web:f0c42c137da2bdb69cf116"
 };
 
-firebase.initializeApp(firebaseConfig)
-
-function MyChart(data) {
-  // console.log(data)
-
-  // data = [
-  //   {label: "Pressure",  data: [{x: new Date(0),, y: 1}]}
-  // ]
-  const series = React.useMemo(
-    () => ({
-      showPoints: false
-    }),
-    []
-  )
-
-  const axes = React.useMemo(
-    () => [
-      { primary: true, type: 'time', position: 'bottom' },
-      { type: 'linear', position: 'left', stacked: false }
-    ],
-    []
-  )
-
-  return (
-
-    <div
-      style={{
-        width: '800px',
-        height: '300px',
-        background: 'rgba(0, 27, 45, 0.9)'
-      }}
-    >
-      <Chart data={data} series={series} axes={axes} tooltip dark />
-    </div>
-  )
-}
+firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.series = React.useMemo(
-      () => ({
-        showPoints: false
-      }),
-      []
-    )
-
-    this.axes = React.useMemo(
-      () => [
-        { primary: true, type: 'time', position: 'bottom' },
-        { type: 'linear', position: 'left', stacked: false }
-      ],
-      []
-    )
-  }
-
   state = {
-    data: [
-      { label: "Pressure", data: [{x: new Date(0), y: 1}]}
-    ]
-  }
+    data: [{ label: "Pressure", data: [{ x: new Date(0), y: 1 }] }]
+  };
 
-  componentDidMount(){
-    const myData = []
-    const db = firebase.database().ref("device/bmp280-01/pressure")
-    db.on('value', snapshot => {
+  componentDidMount() {
+    const myData = [];
+    const db = firebase.database().ref("device/bmp280-01/pressure");
+    db.on("value", snapshot => {
       snapshot.forEach(ref => {
-        // console.log(ref.key)
-        const _da = new Date(0)
-        _da.setUTCSeconds(ref.key)
-        myData.push({ x: _da, y: ref.val()})
-      })
+        const _da = new Date(0);
+        _da.setUTCSeconds(ref.key);
+
+        myData.push({ x: _da, y: Math.round(ref.val()) });
+      });
       this.setState({
-        data: [{
-          label: "Pressure",
-          data: myData
-        }]
-      })
+        data: [
+          {
+            label: "Pressure",
+            data: myData
+          }
+        ]
+      });
     });
   }
 
-  
-
-  
-
   render() {
-    const {data} = this.state
-    console.log(this.axes)
+    const { data } = this.state;
+    const axes = [
+      { primary: true, type: "time", position: "bottom" },
+      { type: "linear", position: "left", stacked: false }
+    ];
+
+    const series = {
+      showPoints: true
+    };
+
     return (
       <div>
         <div
           style={{
-            width: '800px',
-            height: '300px',
-            background: 'rgba(0, 27, 45, 0.9)'
+            width: "800px",
+            height: "300px",
+            background: "rgba(0, 27, 45, 0.9)"
           }}
         >
-        <Chart data={data} series={this.series} axes={this.axes} tooltip dark />
+          <Chart data={data} series={series} axes={axes} tooltip dark />
         </div>
-        {/* <MyChart data={data}></MyChart> */}
       </div>
     );
   }
